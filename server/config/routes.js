37c17116438bdeaed1e5b7d946601f55a37ec801
Rachel RoseFigura <rachel@Liams-MacBook-Pro.local>
2016-01-04 16:38:25 -0800

    commented out unecessary files

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,7 +2,7 @@ var backendTopicController = require('../backendTopics/backendTopicController.js
 
 var backendStoryController = require('../backendTopics/backendStoryController.js');
 var backendOGImageController = require('../backendTopics/backendOGImageController.js');
-var helpers = require('./helpers.js'); // our custom middleware
+// var helpers = require('./helpers.js'); // our custom middleware
 
 module.exports = function (app, express) {
 	
@@ -12,7 +12,7 @@ module.exports = function (app, express) {
 
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
-  app.use(helpers.errorLogger);
-  app.use(helpers.errorHandler);
+  // app.use(helpers.errorLogger);
+  // app.use(helpers.errorHandler);
 };
 
