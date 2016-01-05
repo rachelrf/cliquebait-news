8acbb2ef48ebab65a065308ed6f472a9ca30f4c1
Rachel RoseFigura <rachel@Liams-MacBook-Pro.local>
2016-01-04 19:46:32 -0800

    Add check to see if og link is image

--- server/backendTopics/backendOGImageController.js
+++ server/backendTopics/backendOGImageController.js
@@ -6,18 +6,20 @@ module.exports = {
   fetchOGImages: function (req, res, next) {
     
     var site = req.query.site; //not sure about this
-    var options = {'url': site};
+    var options = {'url': site, 'timeout':2000};
 
     ogs(options, function(err, results) {
         if (err) {
             console.log(err);
         }
-        var newUrlPath = results.data.ogImage;
+
         var newUrl = 'http://wac.450f.edgecastcdn.net/80450F/hudsonvalleycountry.com/files/2015/01/cat4.jpg';
-        if (newUrlPath) {
-          newUrl = results.data.ogImage.url;
+        var regex = /\.(jpg|gif|png)$/;
+        // 
+        if (results.data.ogImage.url && regex.test(results.data.ogImage.url) ) {
+            newUrl = results.data.ogImage.url;
         }
-        
+    
         res.redirect(newUrl);
     });
   
