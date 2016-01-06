e5d4f26b07dfa0f96a199d532e5c29d41168f1f1
Rachel RoseFigura <rachel.rosefigura@gmail.com>
2016-01-05 22:18:40 -0800

    Use MONGOLAB_URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,7 @@
 
 var mongoose = require('mongoose');
 
-if (process.env.NODE_ENV === 'production') {
-	mongoose.connect('mongodb://pique:ADbmvKsGARyFl1IN5DvjZlVpyVUpQjrxfuBBvV82Iuc-@ds058048.mongolab.com:58048/pique');
-else {
-	mongoose.connect('mongodb://localhost/pique');
-}
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/pique');
+
 
 module.exports = mongoose;
